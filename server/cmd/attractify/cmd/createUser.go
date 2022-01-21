3005464d5772572ce209e1811d1c3e05328c839c
TDiercks <tdiercks@inovex.de>
2022-01-21 11:35:31 +0100

    database entry done

--- server/cmd/attractify/cmd/createUser.go
+++ server/cmd/attractify/cmd/createUser.go
@@ -2,8 +2,18 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"attractify.io/platform/app"
+	"attractify.io/platform/auth"
+	"attractify.io/platform/config"
+	"attractify.io/platform/db"
+	"github.com/gofrs/uuid"
+	"github.com/jmoiron/sqlx"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	_ "github.com/lib/pq"
 )
 
 var createUserCmd = &cobra.Command{
@@ -21,8 +31,46 @@ var createUserCmd = &cobra.Command{
 			fmt.Println(err.Error())
 		}
 
-		fmt.Println(name)
-		fmt.Println(email)
+		logger, err := zap.NewProduction()
+		if err != nil {
+			panic(err)
+		}
+		app := app.App{Logger: logger}
+
+		if len(os.Args) <= 1 {
+			panic("missing config")
+		}
+
+		cfgPath := "/app/config.json"
+		app.Config, err = config.Parse(cfgPath)
+		if err != nil {
+			panic(err)
+		}
+
+		dbConn, err := sqlx.Open("postgres", app.Config.DB)
+		if err != nil {
+			panic(err)
+		}
+		app.DB = db.New(dbConn)
+
+		pw := auth.NewPassword("123")
+		ua := db.CreateUserParams{
+			OrganizationID: uuid.FromStringOrNil("bc70b33d-c77f-4fe3-813d-a2605c0915cb"),
+			Email:          email,
+			Password:       pw.Password,
+			Salt:           pw.Salt,
+			Name:           name,
+			Role:           db.RoleAdmin,
+		}
+
+		user, row := app.DB.CreateCLIUser(ua)
+		if row != nil {
+			panic(row.Error())
+		}
+
+		println("User " + user.Name + " mit der E-mail " + user.Email + " wurde angelegt")
+
+		app.DB.Close()
 	},
 }
 
