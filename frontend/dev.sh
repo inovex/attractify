69cc70f035b25180a02dab77654e3609740040a6
TDiercks <tdiercks@inovex.de>
2022-02-08 10:16:05 +0100

    dev setup

--- frontend/dev.sh
+++ frontend/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v "$PWD":/frontend:delegated -w /frontend -p 8080:8080 --entrypoint yarn node:current-alpine3.11 serve
+docker run --rm -it -v "$PWD":/frontend:delegated -w /frontend -p 3000:3000 --entrypoint yarn node:current-alpine3.11 serve
---
+++ server/dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go run cmd/server/main.go config.dev.json
