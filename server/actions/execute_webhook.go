726d9ed1da6b9d83e4579c8e58b185eeecc66a6e
Marc Boeker <marc@at6.net>
2022-08-31 12:36:39 +0200

    Add result debugging

--- server/actions/execute_webhook.go
+++ server/actions/execute_webhook.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
 )
 
 const timeout = time.Second * 30
@@ -94,6 +95,8 @@ func (h Hook) ExecuteWebhook() (*result, error) {
 	body, err := ioutil.ReadAll(webhookRes.Body)
 	res.StatusCode = webhookRes.StatusCode
 	res.Body = string(body)
+
+	h.App.Logger.Debug("actions.webhook", zap.Any("result", res))
 
 	if config.ReturnResult {
 		return &res, err
