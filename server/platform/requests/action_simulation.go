638fedac51988324c530c632828c02e2ad69015c
Tim Diercks <97282004+TDiercks@users.noreply.github.com>
2022-12-12 15:09:28 +0100

    Update action_simulation.go

--- server/platform/requests/action_simulation.go
+++ server/platform/requests/action_simulation.go
@@ -6,8 +6,6 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-// `json:"name" binding:"required,min=1"`
-
 type ActionSimulation struct {
 	User ActionSimulationUser `json:"user" binding:"required"`
 }
@@ -16,7 +14,6 @@ type ActionSimulationUser struct {
 	UserID   uuid.UUID `json:"userId"`
 	Channel  string    `json:"channel"`
 	ActionID string    `json:"actionId"`
-	//Time           int64           `json:"time"`
 	ComputedTraits json.RawMessage `json:"computedTraits"`
 	CustomTraits   json.RawMessage `json:"customTraits"`
 	Context        json.RawMessage `json:"context"`
