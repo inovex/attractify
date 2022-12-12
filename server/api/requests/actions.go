9c87bdd542648a16908bc1c56d5e17dd66a6df9a
TDiercks <tdiercks@inovex.de>
2022-12-12 14:18:30 +0100

    fix request

--- server/api/requests/actions.go
+++ server/api/requests/actions.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Actions struct {
-	UserID   string          `form:"userId" binding:"required,min=1"`
-	TypeName string          `form:"type" binding:"omitempty,min=1"`
-	Tags     []string        `form:"tags" binding:"omitempty,dive,min=1"`
-	Context  json.RawMessage `form:"context" binding:"omitempty,min=1"`
+	UserID   string          `form:"userId" json:"userId" binding:"required,min=1"`
+	TypeName string          `form:"type" json:"type" binding:"omitempty,min=1"`
+	Tags     []string        `form:"tags" json:"tags" binding:"omitempty,dive,min=1"`
+	Context  json.RawMessage `json:"context" binding:"omitempty,min=1"`
 }
 
 type ActionsAct struct {
-	ActionID   string           `form:"actionId" binding:"required,min=1"`
-	UserID     string           `form:"userId" binding:"required,min=1"`
-	Event      string           `form:"event" binding:"required,oneof=show hide decline accept"`
+	ActionID   string           `json:"actionId" binding:"required,min=1"`
+	UserID     string           `json:"userId" binding:"required,min=1"`
+	Event      string           `json:"event" binding:"required,oneof=show hide decline accept"`
 	Context    *json.RawMessage `json:"context" binding:"omitempty"`
 	Properties *json.RawMessage `json:"properties" binding:"omitempty"`
 }
