b09588e7be0c60e12928dfd556dd7737bfb38691
TDiercks <tdiercks@inovex.de>
2022-12-14 16:00:48 +0100

    fix requests

--- server/api/requests/identify.go
+++ server/api/requests/identify.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Identify struct {
-	UserID         string           `json:"userId" binding:"required,min=1"`
-	PreviousUserID string           `json:"previousUserId" binding:"omitempty,min=1"`
-	Type           string           `json:"type" binding:"required,min=1"`
-	IsAnonymous    bool             `json:"isAnonymous" binding:"omitempty"`
-	Traits         *json.RawMessage `json:"traits" binding:"omitempty"`
+	UserID         string           `form:"userId" binding:"required,min=1"`
+	PreviousUserID string           `form:"previousUserId" binding:"omitempty,min=1"`
+	Type           string           `form:"type" binding:"required,min=1"`
+	IsAnonymous    bool             `form:"isAnonymous" binding:"omitempty"`
+	Traits         *json.RawMessage `form:"traits" binding:"omitempty"`
 }
--- server/api/requests/track.go
+++ server/api/requests/track.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Track struct {
-	UserID     string           `json:"userId" binding:"required,min=1"`
-	Event      string           `json:"event" binding:"required,min=1"`
+	UserID     string           `form:"userId" binding:"required,min=1"`
+	Event      string           `form:"event" binding:"required,min=1"`
 	Context    *json.RawMessage `json:"context" binding:"omitempty"`
 	Properties *json.RawMessage `json:"properties" binding:"omitempty"`
 }
