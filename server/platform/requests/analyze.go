d55f74ba05a0d53f4afa7e476f06ecc3df92861e
TDiercks <tdiercks@inovex.de>
2022-12-14 16:12:11 +0100

    fix analyze request

--- server/platform/requests/analyze.go
+++ server/platform/requests/analyze.go
@@ -1,8 +1,8 @@
 package requests
 
 type Analyze struct {
-	ActionID string `json:"actionId" validate:"required,uuid"`
-	Start    string `json:"start" validate:"omitempty,datetime=2006-01-02"`
-	End      string `json:"end" validate:"omitempty,datetime=2006-01-02"`
-	Interval string `json:"interval" validate:"omitempty,oneof=year month day week_day hour"`
+	ActionID string `form:"actionId" validate:"required,uuid"`
+	Start    string `form:"start" validate:"omitempty,datetime=2006-01-02"`
+	End      string `form:"end" validate:"omitempty,datetime=2006-01-02"`
+	Interval string `form:"interval" validate:"omitempty,oneof=year month day week_day hour"`
 }
