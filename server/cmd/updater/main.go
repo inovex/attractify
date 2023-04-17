6f7fd1bcd41f134b406974da935ae82ddd8a3d31
Tim Diercks <97282004+TDiercks@users.noreply.github.com>
2023-04-17 14:01:40 +0200

    added audienceUpdater (#65)
    * added audienceUpdater
    
    * remove contex cancel

---
+++ server/cmd/updater/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"attractify.io/platform/analytics"
+	"attractify.io/platform/app"
+	"attractify.io/platform/audiences"
+	"attractify.io/platform/config"
+	"attractify.io/platform/db"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	defaultConfig = "/run/secrets/config.json"
+)
+
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	app := app.App{Logger: logger}
+
+	cfgPath := defaultConfig
+	if len(os.Args) > 1 {
+		cfgPath = os.Args[1]
+	}
+	app.Config, err = config.Parse(cfgPath)
+	if err != nil {
+		panic(err)
+	}
+
+	dbConn, err := sqlx.Open("postgres", app.Config.DB)
+	if err != nil {
+		panic(err)
+	}
+	app.DB = db.New(dbConn)
+
+	app.Analytics, err = analytics.OpenDB(app.Config.Analytics)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	log.Println("Starting audience update...")
+
+	a, err := app.DB.GetAllAudiences(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, audience := range a {
+		a := audiences.New(ctx, &app, &audience)
+		if _, err := a.Refresh(); err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("Audience update completed...")
+	app.DB.Close()
+	app.Analytics.Close()
+}
